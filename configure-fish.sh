5263e8fc154e59757a27ca2bb053184166fb42ff
arafalko <46872899+arafalko@users.noreply.github.com>
2021-11-21 18:38:34 +0100

    Poprawiony url do fisha

--- configure-fish.sh
+++ configure-fish.sh
@@ -5,7 +5,7 @@ source ./configure-base.sh
 
 ln -s "$HERE"/fish ~/.config/
 
-curl -L https://get.oh-my.fish > installer.fish
+curl -L https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > installer.fish
 fish installer.fish --yes --noninteractive
 rm installer.fish
 fish -c "omf install fzf robbyrussell"
