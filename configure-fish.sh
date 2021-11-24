64bd2d9525bab267aa56029c02fbf492703907c3
Adam Rafałko <adam.rafalko@gmail.com>
2021-11-24 09:33:43 +0100

    Dodałem z do fish

--- configure-fish.sh
+++ configure-fish.sh
@@ -8,4 +8,4 @@ ln -s "$HERE"/fish ~/.config/
 curl -L https://get.oh-my.fish > installer.fish
 fish installer.fish --yes --noninteractive
 rm installer.fish
-fish -c "omf install fzf robbyrussell"
+fish -c "omf install fzf robbyrussell z"
