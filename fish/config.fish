85c3ed90c63da2f99bf2c6baf20a6939fcfc6c91
Adam Rafałko <adam.rafalko@gmail.com>
2022-01-29 19:55:47 +0100

    Plugin do czasu trwania operacji fisha

--- fish/config.fish
+++ fish/config.fish
@@ -13,3 +13,9 @@ fish_user_key_bindings
 
 # fzf bindings for alt-j/k navigation
 export FZF_DEFAULT_OPTS='--bind=alt-j:down,alt-k:up'
+
+# fish command timer
+set fish_command_timer_color yellow
+set fish_command_timer_millis 0
+set fish_command_timer_min_cmd_duration 1000
+set fish_command_timer_time_format '%H:%M:%S'
