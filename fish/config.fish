4f17737a39a74e2037f537824d93017985f509d9
Adam Rafałko <adam.rafalko@gmail.com>
2022-02-11 23:21:22 +0100

    Długość w fish 8

--- fish/config.fish
+++ fish/config.fish
@@ -21,4 +21,4 @@ set fish_command_timer_min_cmd_duration 1000
 set fish_command_timer_time_format '%H:%M:%S'
 
 # fist_prompt_pwd
-set fish_prompt_pwd_dir_length 16
+set fish_prompt_pwd_dir_length 6
