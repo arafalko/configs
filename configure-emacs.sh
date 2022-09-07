ae6c750e60242a5f21f33d59986b28278434dbea
Adam Rafałko <adam.rafalko@gmail.com>
2022-09-08 00:44:03 +0200

    Emacs na CI

--- configure-emacs.sh
+++ configure-emacs.sh
@@ -4,6 +4,6 @@ set -e
 source ./configure-base.sh
 
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-~/.emacs.d/bin/doom install
+~/.emacs.d/bin/doom install --force
 
 ln -s "$HERE"/emacs/doom/doom.d ~/.doom.d
