7c950f2c8a3401a5784c51a5ef05ac7c39711650
Adam Rafałko <adam.rafalko@gmail.com>
2023-02-05 18:20:47 +0100

    Poprawka instalacji xfce

--- configure-xfce4.sh
+++ configure-xfce4.sh
@@ -3,7 +3,8 @@ set -e
 
 source ./configure-base.sh
 
-rmdir --force --recursive ~/.config/xfce4/xfconf
+rm -rf ~/.config/xfce4/xfconf
+rmdir ~/.config/xfce4/xfconf
 mkdir -p ~/.config/xfce4/xfconf 
 
 ln -s "$HERE"/xfce4/xfconf ~/.config/xfce4/xfconf
