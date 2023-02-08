d0bb56bb1f69b8a3b230717ef45e1459f706eb79
radamko <adam.rafalko@gmail.com>
2023-02-08 19:05:27 +0100

    Exa jako ls gdy jest zainstalowany

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,4 +1,12 @@
 # Defined in - @ line 1
-function ls --wraps='ls -alh' --wraps='ls -alh --color=auto' --description 'alias ls ls -alh --color=auto'
- command ls -alh --color=auto $argv;
+#function ls --wraps='ls -alh' --wraps='ls -alh --color=auto' --description 'alias ls ls -alh --color=auto'
+# command ls -alh --color=auto $argv;
+#end
+
+function ls
+    if command -q exa
+       exa -al $argv
+    else
+       ls -alh $argv
+    end
 end
