cb8a4525a40a2c9b39ba23f5574f062475050bb7
Adam Rafałko <adam.rafalko@gmail.com>
2023-04-28 13:23:09 +0200

    ToggleTerm pod Spacja-t

--- vim/lunarvim/config.lua
+++ vim/lunarvim/config.lua
@@ -4,13 +4,36 @@ lvim.keys.insert_mode["ii"] = "<Esc>"
 lvim.builtin.telescope.pickers.find_files = {"truncate"}
 lvim.builtin.telescope.defaults.path_display = {"truncate"}
 
+lvim.builtin.terminal.active = true
+lvim.builtin.terminal.insert_mappings = true
+--lvim.keys.normal_mode["<C-t>"] = "<cmd>ToggleTerm direction=float<CR>"
+lvim.builtin.which_key.mappings["t"] = { "<cmd>ToggleTerm direction=float<CR>", "Terminal" }
+
 -- Additional Plugins
 lvim.plugins = {
-    {
-      "tpope/vim-surround",
-      setup = function()
-         vim.o.timeoutlen = 500
-       end
+  {
+    "tpope/vim-surround",
+    setup = function()
+       vim.o.timeoutlen = 500
+     end
+  },
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit"
     },
+    ft = {"fugitive"}
+  },
 }
-
