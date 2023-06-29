fa57a6727dfcb8030c4656db941f94aa4dc3b7e7
Adam Rafałko <adam.rafalko@gmail.com>
2023-06-29 15:53:05 +0200

    Relativenumbers jako domyślne

--- vim/lunarvim/config.lua
+++ vim/lunarvim/config.lua
@@ -4,10 +4,19 @@ lvim.keys.insert_mode["ii"] = "<Esc>"
 lvim.builtin.telescope.pickers.find_files = {"truncate"}
 lvim.builtin.telescope.defaults.path_display = {"truncate"}
 
+lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
+
 lvim.builtin.terminal.active = true
 lvim.builtin.terminal.insert_mappings = true
+if package.config:sub(1,1) == "\\" then
+  -- windows
+  lvim.builtin.terminal.shell = "pwsh"
+end
 --lvim.keys.normal_mode["<C-t>"] = "<cmd>ToggleTerm direction=float<CR>"
 lvim.builtin.which_key.mappings["t"] = { "<cmd>ToggleTerm direction=float<CR>", "Terminal" }
+
+vim.opt.relativenumber = true
 
 -- Additional Plugins
 lvim.plugins = {
